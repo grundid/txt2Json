0d1eb89cdafe90894261a638fd542dffca736c39
Adrian Stabiszewski <as@grundid.de>
2014-11-30 10:49:11 +0100

    Initial Commit

---
+++ txt2Json/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  txt2Json
+//
+//  Created by Adrian Stabiszewski on 30.11.14.
+//  Copyright (c) 2014 GrundID GmbH. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
